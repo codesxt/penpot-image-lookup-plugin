6fa5239ebfd9acc73d3b206edd46593de4eeb949
Bruno Alexis Faúndez Valenzuela <gameover_sxt_nah@hotmail.com>
2024-11-17 22:58:00 -0300

    add: Image size selector. MIT License. Icons.

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,22 @@
 import { ref, onMounted, watchEffect } from 'vue';
 import { signal } from 'vue-signals';
 
-const theme = signal<string | null>(null);
-const searchInput = ref('');
-const searchResult = ref('');
+const theme = signal<string | null>(null)
+const searchInput = ref('')
+const searchResult = ref('')
 const apiKey = import.meta.env.VITE_PEXELS_API_KEY
 const photos = ref([])
+const size = ref('medium')
+const sizeOptions = ref([
+  { key: 'original', name: 'Original' },
+  { key: 'large2x', name: 'Large 2x'},
+  { key: 'large', name: 'Large'},
+  { key: 'medium', name: 'Medium'},
+  { key: 'small', name: 'Small'},
+  { key: 'portrait', name: 'Portrait' },
+  { key: 'landscape', name: 'Landscape' },
+  { key: 'tiny', name: 'Tiny' }
+])
 
 const page = ref(1)
 const perPage = ref(10)
@@ -76,6 +87,16 @@ const searchMore = async () => {
         Photos provided by <a href="https://pexels.com" target="_blank">Pexels</a>
       </p>
 
+      <div class="form-group" style="margin-bottom: 8px;">
+        <label class="select-label-hidden" for="size-select">Which is your favorite animal?</label>
+        <select class="select" id="size-select" v-model="size">
+          <option disabled>Select image size</option>
+          <option v-for="option of sizeOptions" :value="option.key" :key="option.key">
+            {{ option.name }}
+          </option>
+        </select>
+      </div>
+
       <div class="form-group mb">
         <label class="input-label-hidden" for="input-2">
           Search for images...
@@ -120,7 +141,12 @@ const searchMore = async () => {
         <div v-for="photo in photos" :key="photo.id">
           <div class="image-container">
             <!-- <img :src="photo.src.medium" :alt="photo.alt" class="cover-image"><br> -->
-            <img :src="photo.src.large2x" :alt="photo.alt" class="cover-image"><br>
+            <img
+              :src="photo.src[size]"
+              :alt="photo.alt"
+              class="cover-image"
+            >
+            <br>
           </div>
           <p class="body-s twolines">
             <a :href="photo.url">Photo</a>
